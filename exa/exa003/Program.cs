33663ad10126cc804f0603c5cd602e1449bc1a18
Den <denpeterburg@gmail.com>
2023-12-04 12:23:57 +0300

    все10

--- exa/exa003/Program.cs
+++ exa/exa003/Program.cs
@@ -2,5 +2,5 @@
  int namberA = new Random().Next (1, 10); // случайные числа от 1 до 9 
  Console.WriteLine(namberA);
  int NamberB = new Random().Next (1, 10);
- Console.WriteLine(namberA);
+ Console.WriteLine(NamberB);
  Console.WriteLine(namberA + NamberB);
